52e7b0284432dae9118d351e0f6e3ff9ee655336
Aryan Arora <aryan26arora@gmail.com>
2024-07-01 17:44:42 +0530

    1- post routes and controllers added

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -54,6 +54,15 @@ const userSchema = new mongoose.Schema({
         type: String,
         default: ""
     },
+
+    likedPosts:[
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Post",
+            default: [],
+        },
+    ],
+
 },{timestamps: true});
 
 const User = mongoose.model("User", userSchema);
